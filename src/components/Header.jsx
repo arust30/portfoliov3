9b1997946773fca10b19c4c93ee1ca857c3cafe5
buckets <andrewrust30@gmail.com>
2023-03-30 00:00:34 -0400

    Basic Outline of the website is done

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,60 +1,72 @@
-import React from "react";
+import React, { useState } from "react";
 import ColumbiaPoloHeadshot from "../images/AndrewHeadshotColumbiaPolo.png";
 // import BlueTieSuitHeadshot from "../images/AndrewHeadshotBlueTieSuit.png";
-import "./wave.css"
+import "./wave.css";
+import { ArrowDownTrayIcon, CheckIcon } from "@heroicons/react/24/outline";
 
 function Header() {
+  const [isDownloaded, setIsDownloaded] = useState(false);
+
+  const handleDownload = () => {
+    setIsDownloaded(true);
+    //wait 5 seconds
+    setTimeout(() => {
+      setIsDownloaded(false);
+    }, 5000);
+  };
   return (
     <>
-        <div class="grid max-w-screen-xl h-[80vh] px-4 py-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12">
-          <div class="mr-auto place-self-center lg:col-span-7">
-            <h1 class="max-w-2xl mb-8 text-4xl font-extrabold tracking-tight leading-none md:text-5xl xl:text-6xl">
-              Andrew Rust
-            </h1>
-            <p class="max-w-2xl mb-8 font-light text-gray lg:mb-8 md:text-lg lg:text-xl">
-              I am a full-stack web developer with a passion for building stuff
-              and helping people.
-            </p>
-            <a
-              href="/"
-              class="inline-flex items-center justify-center px-5 py-3 mr-6 text-base font-medium text-center text-white rounded-lg bg-primary hover:bg-primary focus:ring-4 focus:ring-primary"
-            >
-              Download Resume
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke-width="1.5"
-                stroke="currentColor"
-                class="w-5 h-5 ml-2 -mr-1"
-              >
-                <path
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  d="M3 16.5v2.25A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75V16.5M16.5 12L12 16.5m0 0L7.5 12m4.5 4.5V3"
-                />
-              </svg>
-            </a>
-            <a
-              href="/"
-              class="inline-flex items-center justify-center px-5 py-3 text-base font-medium text-center text-gray border border-black rounded-lg hover:bg-light_gray focus:ring-4 focus:ring-gray"
-            >
-              Contact Me
-            </a>
-          </div>
-          <div class="hidden lg:mt-0 lg:col-span-5 lg:flex">
-            {/* <img
+      <div class="grid max-w-screen-xl h-[75vh] px-4 py-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12">
+        <div class="mr-auto place-self-center lg:col-span-7">
+          <h1 class="max-w-2xl mb-12 text-4xl font-extrabold tracking-tight leading-none md:text-5xl xl:text-6xl">
+            Andrew Rust
+          </h1>
+          <p class="max-w-2xl mb-12 font-light text-gray lg:mb-8 md:text-lg lg:text-xl">
+            I am a full-stack web developer with a passion for building stuff
+            and helping people.
+          </p>
+          <a
+            href="#download"
+            style={{cursor: "pointer"}}
+            onClick={handleDownload}
+            className={`mt-4 inline-flex items-center justify-center px-5 py-3 mr-8 text-base font-medium text-center rounded-lg ${
+              isDownloaded
+                ? "bg-secondary border-secondary text-white_alt hover:secondary hover:border-secondary"
+                : "bg-primary border-primary text-white_alt hover:bg-light_gray hover:text-primary"
+            } border-2`}
+          >
+            {isDownloaded ? (
+              <>
+                Downloaded
+                <CheckIcon className="w-5 h-5 ml-2 -mr-1" />
+              </>
+            ) : (
+              <>
+                Download Resume
+                <ArrowDownTrayIcon className="w-5 h-5 ml-2 -mr-1" />
+              </>
+            )}
+          </a>
+          <a
+            href="/"
+            class="inline-flex items-center justify-center px-5 py-3 text-base font-medium text-center text-gray border border-black rounded-lg hover:bg-light_gray focus:ring-4 focus:ring-gray"
+          >
+            Contact Me
+          </a>
+        </div>
+        <div class="hidden lg:mt-0 lg:col-span-5 lg:flex">
+          {/* <img
               src={BlueTieSuitHeadshot}
               alt="Me in a Columbia polo"
               class="object-cover transition-opacity duration-500 ease-out"
             /> */}
-            <img
-              src={ColumbiaPoloHeadshot}
-              alt="Me in a blue tie and suit"
-              class="object-cover transition-opacity duration-500 ease-out opacity-100 hover:opacity-0"
-            />
-          </div>
+          <img
+            src={ColumbiaPoloHeadshot}
+            alt="Me in a blue tie and suit"
+            class="object-cover transition-opacity duration-500 ease-out opacity-100 hover:opacity-0"
+          />
         </div>
+      </div>
     </>
   );
 }
